b2f7930f81e9f28401cb4da3cf7da6e0bfc30592
Javier Figueroa <javiernfigueroa@gmail.com>
2024-12-03 20:52:01 -0300

    ff

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,3 @@
-import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -24,7 +23,6 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
-      ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
@@ -33,6 +31,26 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      // Asegura que todas las variables declaradas sean utilizadas. Esto ayuda a mantener el código limpio.
+      'no-unused-vars': 'error',
+
+      // Obliga a usar comillas simples (`'`) en lugar de dobles (`"`) para strings en el código JavaScript.
+      'quotes': ['error', 'single', { allowTemplateLiterals: true }],
+
+      // Obliga a usar comillas simples en las propiedades JSX como `className` o `id`.
+      'jsx-quotes': ['error', 'prefer-single'],
+
+      // Prohíbe el uso de punto y coma (`;`) al final de las líneas.
+      'semi': ['error', 'never'],
+
+      // Obliga a usar funciones flecha en callbacks o funciones anónimas.
+      'prefer-arrow-callback': 'error',
+
+      // Obliga a declarar todas las funciones como expresiones (por ejemplo, `const App = () => {}`) en lugar de funciones declaradas (`function App() {}`).
+      'func-style': ['error', 'expression'],
+
+      // Obliga a tener un espacio antes y después de la flecha (`=>`) en funciones flecha para mejorar la legibilidad.
+      'arrow-spacing': ['error', { before: true, after: true }],
     },
   },
 ]
